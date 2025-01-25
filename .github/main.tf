1f38de67ecee88aa21149ce8abe0e8ff3542a5b7
PREETI <99242310+preetsajjan@users.noreply.github.com>
2025-01-25 18:02:17 +0530

    Update main.tf

--- .github/main.tf
+++ .github/main.tf
@@ -22,3 +22,53 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
+resource "snowflake_table" "sensor" {
+  provider = snowflake
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.demo_schema.name
+  name     = "WEATHER_JSON"
+  column {
+    name    = "var"
+    type    = "VARIANT"
+    comment = "Raw sensor data"
+  }
+}
+resource "snowflake_file_format" "json" {
+  provider             = snowflake
+  name                 = "JSON_FORMAT"
+  database             = snowflake_database.demo_db.name
+  schema               = snowflake_schema.demo_schema.name
+  format_type          = "JSON"
+  strip_outer_array    = true
+  compression          = "NONE"
+  binary_format        = "HEX"
+  date_format          = "AUTO"
+  time_format          = "AUTO"
+  timestamp_format     = "AUTO"
+  skip_byte_order_mark = true
+}
+resource "snowflake_stage" "example_stage" {
+  name        = "EXAMPLE_STAGE"
+  url         = "s3://snowflake-nse-data/"
+  database    = "DEMO_DB"
+  schema      = "DEMO_SCHEMA"
+  credentials =  "AWS_KEY_ID='${var.access_key}' AWS_SECRET_KEY='${var.secret_key}'"
+}
+resource "snowflake_view" "view" {
+  database = "DEMO_DB"
+  schema   = "DEMO_SCHEMA"
+  name     = "NEW_VIEW"
+
+  comment = "comment"
+
+  statement  = <<-SQL
+    select * from WEATHER_JSON;
+SQL
+  or_replace = false
+  is_secure  = false
+}
