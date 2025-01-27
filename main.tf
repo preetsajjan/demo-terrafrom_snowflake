1fe7399256340631fa7b2aa17dd8cd6d5036ea8a
PREETI <99242310+preetsajjan@users.noreply.github.com>
2025-01-27 15:31:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,3 +49,17 @@ resource "snowflake_table" "sensor" {
     comment = "Raw sensor data"
   }
 }
+
+resource "snowflake_file_format" "json" {
+  name                 = "JSON_FORMAT"
+  database             = snowflake_database.demo_db.name
+  schema               = snowflake_schema.demo_schema.name
+  format_type          = "JSON"
+  strip_outer_array    = true
+  compression          = "NONE"
+  binary_format        = "HEX"
+  date_format          = "AUTO"
+  time_format          = "AUTO"
+  timestamp_format     = "AUTO"
+  skip_byte_order_mark = true
+}
