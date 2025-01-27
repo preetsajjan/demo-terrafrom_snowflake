2436bff73feb037ca270471f7e27fe83af1609e6
PREETI <99242310+preetsajjan@users.noreply.github.com>
2025-01-27 11:59:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,23 +16,31 @@ terraform {
 }
 
 provider "snowflake" {
-SNOWFLAKE_REGION = "singapore_asia_pacific"
-SNOWFLAKE_ACCOUNT = "HR31688"
-SNOWFLAKE_USER = "PREETIMITRA"
-SNOWFLAKE_PASSWORD = "Preetimitra@2024"
+  account   = var.snowflake_account
+  user      = var.snowflake_user
+  password  = var.snowflake_password
+  region    = var.snowflake_region
 }
+
+variable "snowflake_account" {}
+variable "snowflake_user" {}
+variable "snowflake_password" {}
+variable "snowflake_region" {}
+variable "access_key" {}
+variable "secret_key" {}
 
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
+
 resource "snowflake_table" "sensor" {
-  provider = snowflake
   database = snowflake_database.demo_db.name
   schema   = snowflake_schema.demo_schema.name
   name     = "WEATHER_JSON"
@@ -42,8 +50,8 @@ resource "snowflake_table" "sensor" {
     comment = "Raw sensor data"
   }
 }
+
 resource "snowflake_file_format" "json" {
-  provider             = snowflake
   name                 = "JSON_FORMAT"
   database             = snowflake_database.demo_db.name
   schema               = snowflake_schema.demo_schema.name
@@ -56,23 +64,28 @@ resource "snowflake_file_format" "json" {
   timestamp_format     = "AUTO"
   skip_byte_order_mark = true
 }
+
 resource "snowflake_stage" "example_stage" {
   name        = "EXAMPLE_STAGE"
   url         = "s3://snowflake-nse-data/"
-  database    = "DEMO_DB"
-  schema      = "DEMO_SCHEMA"
-  credentials =  "AWS_KEY_ID='${var.access_key}' AWS_SECRET_KEY='${var.secret_key}'"
+  database    = snowflake_database.demo_db.name
+  schema      = snowflake_schema.demo_schema.name
+  credentials = <<-EOT
+    AWS_KEY_ID='${var.access_key}' AWS_SECRET_KEY='${var.secret_key}'
+  EOT
 }
+
 resource "snowflake_view" "view" {
-  database = "DEMO_DB"
-  schema   = "DEMO_SCHEMA"
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.demo_schema.name
   name     = "NEW_VIEW"
 
   comment = "comment"
 
-  statement  = <<-SQL
+  statement = <<-SQL
     select * from WEATHER_JSON;
-SQL
+  SQL
+
   or_replace = false
   is_secure  = false
 }
